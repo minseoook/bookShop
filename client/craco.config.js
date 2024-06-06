c0b440b489bad73a38f8208b6b69dca2cf0fa45f
minseok <imuchgabis@gmail.com>
2024-06-06 17:37:12 +0900

    크라코로 절대경로 적용

---
+++ client/craco.config.js
@@ -0,0 +1,15 @@
+const cracoAlisa = require("craco-alias");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: cracoAlisa,
+      options: {
+        source: "tsconfig",
+        baseurl: ".",
+        tsConfigPath: "tsconfig.paths.json",
+        debug: false,
+      },
+    },
+  ],
+};
